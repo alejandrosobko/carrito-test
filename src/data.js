6f3743f94ac555843384fb8b047b9546d2b22f38
Alejandro Sobko <alejandro.sobko@gmail.com>
2020-07-05 04:02:45 -0300

    Agrego archivo con data

---
+++ src/data.js
@@ -0,0 +1,218 @@
+export default [
+  {
+    title: 'Producto 1',
+    price: '$200',
+    description: 'Mi producto numero 1',
+    image: 'https://picsum.photos/200/301'
+  },
+  {
+    title: 'Producto 2',
+    price: '$55',
+    description: 'Mi producto numero 2',
+    image: 'https://picsum.photos/200/302'
+  },
+  {
+    title: 'Producto 3',
+    price: '$190',
+    description: 'Mi producto numero 3',
+    image: 'https://picsum.photos/200/303'
+  },
+  {
+    title: 'Producto 4',
+    price: '$450',
+    description: 'Mi producto numero 4',
+    image: 'https://picsum.photos/200/304'
+  },
+  {
+    title: 'Producto 5',
+    price: '$288',
+    description: 'Mi producto numero 5',
+    image: 'https://picsum.photos/200/305'
+  },
+  {
+    title: 'Producto 6',
+    price: '$25',
+    description: 'Mi producto numero 6',
+    image: 'https://picsum.photos/200/306'
+  },
+  {
+    title: 'Producto 1',
+    price: '$200',
+    description: 'Mi producto numero 1',
+    image: 'https://picsum.photos/200/301'
+  },
+  {
+    title: 'Producto 2',
+    price: '$55',
+    description: 'Mi producto numero 2',
+    image: 'https://picsum.photos/200/302'
+  },
+  {
+    title: 'Producto 3',
+    price: '$190',
+    description: 'Mi producto numero 3',
+    image: 'https://picsum.photos/200/303'
+  },
+  {
+    title: 'Producto 4',
+    price: '$450',
+    description: 'Mi producto numero 4',
+    image: 'https://picsum.photos/200/304'
+  },
+  {
+    title: 'Producto 5',
+    price: '$288',
+    description: 'Mi producto numero 5',
+    image: 'https://picsum.photos/200/305'
+  },
+  {
+    title: 'Producto 6',
+    price: '$25',
+    description: 'Mi producto numero 6',
+    image: 'https://picsum.photos/200/306'
+  },
+  {
+    title: 'Producto 1',
+    price: '$200',
+    description: 'Mi producto numero 1',
+    image: 'https://picsum.photos/200/301'
+  },
+  {
+    title: 'Producto 2',
+    price: '$55',
+    description: 'Mi producto numero 2',
+    image: 'https://picsum.photos/200/302'
+  },
+  {
+    title: 'Producto 3',
+    price: '$190',
+    description: 'Mi producto numero 3',
+    image: 'https://picsum.photos/200/303'
+  },
+  {
+    title: 'Producto 4',
+    price: '$450',
+    description: 'Mi producto numero 4',
+    image: 'https://picsum.photos/200/304'
+  },
+  {
+    title: 'Producto 5',
+    price: '$288',
+    description: 'Mi producto numero 5',
+    image: 'https://picsum.photos/200/305'
+  },
+  {
+    title: 'Producto 6',
+    price: '$25',
+    description: 'Mi producto numero 6',
+    image: 'https://picsum.photos/200/306'
+  },
+  {
+    title: 'Producto 1',
+    price: '$200',
+    description: 'Mi producto numero 1',
+    image: 'https://picsum.photos/200/301'
+  },
+  {
+    title: 'Producto 2',
+    price: '$55',
+    description: 'Mi producto numero 2',
+    image: 'https://picsum.photos/200/302'
+  },
+  {
+    title: 'Producto 3',
+    price: '$190',
+    description: 'Mi producto numero 3',
+    image: 'https://picsum.photos/200/303'
+  },
+  {
+    title: 'Producto 4',
+    price: '$450',
+    description: 'Mi producto numero 4',
+    image: 'https://picsum.photos/200/304'
+  },
+  {
+    title: 'Producto 5',
+    price: '$288',
+    description: 'Mi producto numero 5',
+    image: 'https://picsum.photos/200/305'
+  },
+  {
+    title: 'Producto 6',
+    price: '$25',
+    description: 'Mi producto numero 6',
+    image: 'https://picsum.photos/200/306'
+  },
+  {
+    title: 'Producto 1',
+    price: '$200',
+    description: 'Mi producto numero 1',
+    image: 'https://picsum.photos/200/301'
+  },
+  {
+    title: 'Producto 2',
+    price: '$55',
+    description: 'Mi producto numero 2',
+    image: 'https://picsum.photos/200/302'
+  },
+  {
+    title: 'Producto 3',
+    price: '$190',
+    description: 'Mi producto numero 3',
+    image: 'https://picsum.photos/200/303'
+  },
+  {
+    title: 'Producto 4',
+    price: '$450',
+    description: 'Mi producto numero 4',
+    image: 'https://picsum.photos/200/304'
+  },
+  {
+    title: 'Producto 5',
+    price: '$288',
+    description: 'Mi producto numero 5',
+    image: 'https://picsum.photos/200/305'
+  },
+  {
+    title: 'Producto 6',
+    price: '$25',
+    description: 'Mi producto numero 6',
+    image: 'https://picsum.photos/200/306'
+  },
+  {
+    title: 'Producto 1',
+    price: '$200',
+    description: 'Mi producto numero 1',
+    image: 'https://picsum.photos/200/301'
+  },
+  {
+    title: 'Producto 2',
+    price: '$55',
+    description: 'Mi producto numero 2',
+    image: 'https://picsum.photos/200/302'
+  },
+  {
+    title: 'Producto 3',
+    price: '$190',
+    description: 'Mi producto numero 3',
+    image: 'https://picsum.photos/200/303'
+  },
+  {
+    title: 'Producto 4',
+    price: '$450',
+    description: 'Mi producto numero 4',
+    image: 'https://picsum.photos/200/304'
+  },
+  {
+    title: 'Producto 5',
+    price: '$288',
+    description: 'Mi producto numero 5',
+    image: 'https://picsum.photos/200/305'
+  },
+  {
+    title: 'Producto 6',
+    price: '$25',
+    description: 'Mi producto numero 6',
+    image: 'https://picsum.photos/200/306'
+  },
+];
